8c54cbc7549378985cf933c4f3a8e5a9e86601a5
Joyce Padela <joycepadela@gmail.com>
2016-09-23 14:00:41 -0400

    PA2 Created matrix, cached and reversed Solve Funct

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This second programming assignment is to create two funtions to demo the new
+## <<- assignment operator for setting a variable outside of the current environment.
+## It is also used to demo how cached objects are computed much faster and to optimize
+## the processing power with R.
+## The first function is similar to the example makeVector provided in the assignment
+## instructions.  
 
-## Write a short comment describing this function
+## makeCacheMatrix gets a matrix input values (set then get values) and creates the 
+## inverse sets and then creates the inverse using solve() and stores it (assuming it is 
+## stored in the cache).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    matx <- NULL
+    set <- function(y) {
+        x <<- y
+        matx <<- NULL
+    }
+    get <- function() x
+    #Create / Calculate the inverse using solve()
+    setInverse <- function() matx <<- solve(x) 
+    getInverse <- function() matx
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## The following function takes the matrix created in makeCacheMatrix
+## above and checks for its existence and prints it if it does exist.  If 
+## it doesn't exist, it saves the matrix and prints the inverse using "setInverse".
+## needs a squared matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    matx <- x$getInverse()
+    if (!is.null(matx)) {
+        message("Get the cached data")
+        return(matx)
+    }
+    data <- x$get()
+    matx <- solve(data, ...)
+    x$setInverse(matx)
+    matx
 }
